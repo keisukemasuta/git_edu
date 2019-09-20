350d8c759d1f5fb4afae02563faa7dc4474486c1
keisukemasuta <kei20000918@gmail.com>
2019-09-20 16:06:23 +0900

    modify named

--- azechi.cs
+++ azechi.cs
@@ -1,11 +1,11 @@
 public class Azechi{
-
+    
     public string name;
 
     public int age;
 
     public Azechi(){
-        name = "ひも";
-        age = 10;
+        name = "あぜっちゃん";
+        age = 21;
     }
 }
